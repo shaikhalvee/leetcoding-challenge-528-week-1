db14b5b197f8e192cf52bb04b68d7a210813f651
Shaikh Islam <shaikhalvee@gmail.com>
2020-05-24 15:50:39 +0600

    skeleton file added

--- 30-day-leetcoding-challenge-528-week-1-3287.cpp
+++ 30-day-leetcoding-challenge-528-week-1-3287.cpp
@@ -3,10 +3,35 @@ using namespace std;
 
 typedef vector<int> vi;
 
+void print (vi);
+int maxProfit (vi&);
+
 int main()
 {
-    vi i;
-    i.push_back(5);
-    cout << i[0];
+    ios_base::sync_with_stdio(0);
+
+    size_t len;
+    vi prices;
+    int val;
+    cin >> len;
+    for (size_t i = 0; i < len; i++)
+        cin >> val, prices.push_back(val);
+    cout << maxProfit(prices);
+    return 0;
+}
+
+void print (vi ara) {
+    for (size_t i = 0; i < ara.size(); i++)
+        cout << ara[i], (i != ara.size() - 1) ? cout << " " : cout << "";
+    cout << "\n";
+}
+
+int maxProfit (vi & prices) {
+    vi trace;
+    trace.assign(prices.size(), -1);
+    trace[trace.size()-1] = 0;
+
+
+
     return 0;
 }
